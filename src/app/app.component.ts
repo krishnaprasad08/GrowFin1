5ddea5b369da106d4142b026e649ac2cd712079f
Krishnaprasad.girira <krishnaprasad.girira@everi.com>
2022-05-28 01:15:36 +0530

    Drop down Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GrowFin';
+  
+  status = new FormControl();
+  Dues = new FormControl();
+  Dates = new FormControl();
+  selectCustom = new FormControl();
+  viewValue: string[] = [ ];
+  paymentList: string[] = ['Payment pending', 'Fully Paid', 'Partially Paid', 'In Dispute', 'Closed'];
+  DueList: string[] = ['This week', 'This month'];
+  DateRange: string[] = ['This week', 'Last 7 days', 'Last 30 days'];
+  showbutton: boolean = true;
+  customList: any = 
+    ['hello', 'Issued Date Range', 'World']
+  ;
+
+  submitForm() {
+    let formObj = {
+      status: this.status.value,
+      Dues: this.Dues.value,
+      Dates: this.Dates.value      
+    }
+    console.log(formObj)
+
+  }
+  hideButton() {
+    this.showbutton = false;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DxButtonModule } from 'devextreme-angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {MatRadioModule} from '@angular/material/radio';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
